07f9a0650372acabeacd2022f4133ff4162b4939
Tushar Mhatre <ttmhatre@gmail.com>
2023-12-15 19:34:48 +0530

    Portfolio website

---
+++ javascript.js
@@ -0,0 +1,24 @@
+var tablinks=document.getElementsByClassName("tab-links")
+var tabcontents=document.getElementsByClassName("tab-contents")
+function opentab(tabname){
+  for(tablink of tablinks){
+    tablink.classList.remove("active-link");
+  }
+   for(tabcontent of tabcontents){
+    tabcontent.classList.remove("active-tab");
+  }
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab");
+}
+
+
+ const scriptURL = 'https://script.google.com/macros/s/AKfycbxHknaENjpLIZjXSwlUgMkNbKRsLH8rEbxCdWH-Cf_UI8kiBLVmnT8jezh6jMJXDSC6Vw/exec'
+
+      const form = document.forms['submit-to-google-sheet']
+
+      form.addEventListener('submit', e => {
+        e.preventDefault()
+        fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+          .then(response => console.log('Success!', response))
+          .catch(error => console.error('Error!', error.message))
+      })
